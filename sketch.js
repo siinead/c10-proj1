a28ec3966924a11e13c0e3a50037c93054e967c2
siinead <93890270+siinead@users.noreply.github.com>
2021-11-14 18:23:03 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var weight=[10,20,30,40,50]
+function setup() 
+{
+  createCanvas(400,400);
+  var sum=weight[0]+weight[1]+weight[2]+weight[3]+weight[4]
+  var avg=sum/weight.length
+  console.log(avg)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
